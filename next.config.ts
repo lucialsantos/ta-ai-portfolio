50ae3dff2082994f35df80c613c5ccdcefb50ecf
lucialsantos <111369470+lucialsantos@users.noreply.github.com>
2026-04-13 22:19:41 +0200

    Update Next.js configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/ta-ai-portfolio',
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
